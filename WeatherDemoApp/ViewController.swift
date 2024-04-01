6778a41c6aefb0f6c0f16d2b382288b69d06938c
Ahmed ibrahim <ahmed-ibrahim-salim>
2024-04-01 09:34:00 +0200

    initial proj, installed pods, linting and .gitignore

---
+++ WeatherDemoApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  WeatherDemoApp
+//
+//  Created by ahmed on 01/04/2024.
+//
+
+import UIKit
+import Realm
+import SVProgressHUD
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
